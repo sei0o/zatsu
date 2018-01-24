223280744eda4d6b490c109ca98a8aaa565486dd
sei0o <sei0o@live.jp>
2018-01-24 23:37:47 +0900

    Implemented recording

--- zatsu.rb
+++ zatsu.rb
@@ -87,10 +87,26 @@ module Zatsu
       end
     end
 
-    def start_recording
+    def switch_task
+      [nil, get_plan, nil].flatten.each_cons(2) do |prev, cur| # 汚い
+        unless cur&.actual_start
+          cur&.update actual_start: Time.zone.now
+          prev&.update actual_duration: (Time.zone.now - prev.actual_start) / 60
+          break
+        end
+      end
     end
 
-    def finish_recording
+    def start_recording
+      get_plan[0].update actual_start: Time.zone.now
+    end
+
+    def review_recording
+      # do nothing yet
+    end
+
+    def recording?
+      get_plan.any? && get_plan[0].actual_start && !get_plan[-1].actual_duration
     end
   end
 
@@ -100,6 +116,11 @@ module Zatsu
 
     desc "plan [arg1:val1, ...]", "plan your schedule for today"
     def plan *args
+      if Manager.recording?
+        print "A recording has already started. Want to stop and create a new plan? (y/n) "
+        return if STDIN.gets.chomp != "y"
+      end
+
       hash = {}
       args.each do |arg|
         hash[arg.split(":")[0]] = arg.split(":")[1]
@@ -117,7 +138,17 @@ module Zatsu
 
     desc "switch [task name]", "switch task"
     def switch task_name = nil
-      # show rest task
+      if Manager.recording?
+        Manager.switch_task
+        Manager.show_status
+
+        unless Manager.recording? # if finished
+          print "You have finished the plan for today. Want to review your record? (y/n) "
+          Manager.review_recording if STDIN.gets.chomp == "y"
+        end
+      else
+        Manager.start_recording
+      end
     end
   end
 end
