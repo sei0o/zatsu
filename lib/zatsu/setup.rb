369e37c05089340b2f663f7171e40909b9238d7e
sei0o <sei0o@live.jp>
2018-11-17 00:03:16 +0900

    Added debug script

--- lib/zatsu/setup.rb
+++ lib/zatsu/setup.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'active_record'
 
 module Zatsu
   ZATSU_DIR = "#{Dir.home}/.zatsu"
@@ -9,24 +10,25 @@ module Zatsu
     return if Dir.exist? ZATSU_DIR
 
     begin
-      Dir.mkdir DATA_DIR, 0766
+      Dir.mkdir ZATSU_DIR, 0766
       open("#{ZATSU_DIR}/log.db", 'w+') {} # Create database file
 
       FileUtils.mkdir ["#{ZATSU_DIR}/generators", "#{ZATSU_DIR}/schedulers"]
-      FileUtils.copy_entry './generators', "#{ZATSU_DIR}/generators"
-      FileUtils.copy_entry './schedulers', "#{ZATSU_DIR}/schedulers"
+      FileUtils.copy_entry "#{__dir__}/generators", "#{ZATSU_DIR}/generators"
+      FileUtils.copy_entry "#{__dir__}/schedulers", "#{ZATSU_DIR}/schedulers"
     rescue Exception => ex
       p ex
     end
 
     ActiveRecord::Base.establish_connection adapter: :sqlite3, database: "#{Dir.home}/.zatsu/log.db"
-    ActiveRecord::Migrator.migrate "#{Dir.home}/.zatsu/log.db", nil
+    # see: https://github.com/padrino/padrino-framework/pull/2182/commits/faf9becab40446346f405b9fda55538a7461a86a
+    ActiveRecord::MigrationContext.new("#{__dir__}/db/migrate/").migrate
   end
 
   def reset_scripts
     FileUtils.rm_rf ["#{ZATSU_DIR}/generators", "#{ZATSU_DIR}/schedulers"]
-    FileUtils.copy_entry './generators', "#{ZATSU_DIR}/generators"
-    FileUtils.copy_entry './schedulers', "#{ZATSU_DIR}/schedulers"
+    FileUtils.copy_entry "#{__dir__}/generators", "#{ZATSU_DIR}/generators"
+    FileUtils.copy_entry "#{__dir__}/schedulers", "#{ZATSU_DIR}/schedulers"
 
     puts "Copied original scripts!"
   end
